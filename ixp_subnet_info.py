995380a7305abe940732c7a4071375325bbae267
Mandar <mandar1997@gmail.com>
2020-09-17 17:42:30 +0200

    Update ixp_subnet_info.py

--- ixp_subnet_info.py
+++ ixp_subnet_info.py
@@ -1,17 +1,27 @@
 import json
 import requests
+#import netaddr
+#import ipaddress
+
+#ask how often peeringdb changes
+
 
 response = requests.get("https://peeringdb.com/api/ixlan?depth=2")
+response2 = requests.get("https://peeringdb.com/api/netixlan")
+
+
+
 facilities = json.loads(response.text)
+netixlan = json.loads(response2.text)
 
-print(facilities == response.json())
+#print(facilities == response.json())
 
-print(type(facilities))
+#print(type(facilities))
 
 dictionary = {}
-dictionary["id"] = []
 
-print(dictionary)
+
+dictionary["id"] = []
 
 for i in facilities["data"]:
     if i["ixpfx_set"]:
@@ -21,6 +31,16 @@ for i in facilities["data"]:
                 dictionary2 = {}
                 dictionary2["ix_id"] = i["ix_id"]
                 dictionary2["ipv4_prefix"] = j["prefix"]
+                dictionary2["net_set"] = []
+                for k in i["net_set"]:
+                    dictionary3 = {}
+                    dictionary3["asn"] = k["asn"]
+                    for l in netixlan["data"]: 
+                        if l["asn"] == k["asn"] and l["ix_id"] == i["ix_id"]:
+                            #print("asn detected")
+                            dictionary3["ipaddr4"] = l["ipaddr4"]
+                            break
+                    dictionary2["net_set"].append(dictionary3)
                 dictionary["id"].append(dictionary2)
 
                 #create a dictionary here with the values we found
@@ -29,3 +49,6 @@ for i in facilities["data"]:
 
 
 print(dictionary)
+
+with open('ixp.json', 'w') as fp:
+        json.dump(dictionary,fp)
