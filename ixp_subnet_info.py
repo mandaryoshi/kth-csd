ec31871a81b5744fde96d97a842fb6a0169126a4
Enric Carrera Aguiar <enricca75@gmail.com>
2020-09-20 17:16:03 +0200

    added ixp to fac mapping

--- ixp_subnet_info.py
+++ ixp_subnet_info.py
@@ -8,12 +8,12 @@ import requests
 
 response = requests.get("https://peeringdb.com/api/ixlan?depth=2")
 response2 = requests.get("https://peeringdb.com/api/netixlan")
-
+response3 = requests.get("https://peeringdb.com/api/ixfac")
 
 
 facilities = json.loads(response.text)
 netixlan = json.loads(response2.text)
-
+ixpfac = json.loads(response3.text)
 #print(facilities == response.json())
 
 #print(type(facilities))
@@ -27,12 +27,13 @@ for i in facilities["data"]:
     if i["ixpfx_set"]:
         for j in i["ixpfx_set"]:
             if j["protocol"] == "IPv4":
-                print(j["prefix"])
+                #print(j["prefix"])
                 dictionary2 = {}
                 dictionary2["ix_id"] = i["ix_id"]
                 dictionary2["ipv4_prefix"] = j["prefix"]
                 dictionary2["net_set"] = []
                 for k in i["net_set"]:
+                    #Dictionary containing the info from the all ASN connected to the IXP // Contains an array of asn_id & IPv4 addr
                     dictionary3 = {}
                     dictionary3["asn"] = k["asn"]
                     for l in netixlan["data"]: 
@@ -41,8 +42,13 @@ for i in facilities["data"]:
                             dictionary3["ipaddr4"] = l["ipaddr4"]
                             break
                     dictionary2["net_set"].append(dictionary3)
+                dictionary2["fac_set"] = []
+                for y in ixpfac["data"]:
+                    if y["ix_id"] == i["ix_id"]:
+                        dictionary2["fac_set"].append(y["fac_id"])
                 dictionary["id"].append(dictionary2)
-
+                
+                
                 #create a dictionary here with the values we found
                 #we want id, ix_id, ipv4 prefix
                 #{"id": [{"ix_id": "xyz", "ipv4_prefix": "xyz"}, {"ix_id": "xyz", "ipv4_prefix": "xyz"}]}
