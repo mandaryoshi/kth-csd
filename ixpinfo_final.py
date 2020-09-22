b8385973f344c9114df74ec761bb4d4873211180
Enric Carrera Aguiar <enricca75@gmail.com>
2020-09-22 15:21:17 +0200

    asn to ip change

--- ixpinfo_final.py
+++ ixpinfo_final.py
@@ -15,28 +15,23 @@ dictionary = {}
 for i in tqdm(ixpfx["data"]):
     if i["ixpfx_set"]:
         for j in i["ixpfx_set"]:
+            dictionary2["ipv4_prefix"] = []
             if j["protocol"] == "IPv4":
 
                 dictionary2 = {}
-                dictionary2["ipv4_prefix"] = j["prefix"]
-
-                x = 0
+                dictionary2["ipv4_prefix"].append(j["prefix"])
                 dictionary3 = {}
+
                 for k in i["net_set"]:
 
                     #Dictionary containing the info from the all ASN connected to the IXP // Contains an array of asn_id & IPv4 addr
                     #dictionary3["asn"] = k["asn"]
 
                     for l in netixlan["data"]: 
-                        
-                        if l["ix_id"] == i["ix_id"] and x != l["ipaddr4"]:
-                            #print("asn detected")
+
+                        if l["ix_id"] == i["ix_id"]:
                             dictionary3[l["ipaddr4"]] = l["asn"]
-                            #dictionary2["net_set"].append(dictionary3)
-                            x = l["ipaddr4"]
-                            
-                            
-                    #dictionary2["net_set"].append(dictionary3)
+                        
                 dictionary2["net_set"] = dictionary3
                 dictionary2["fac_set"] = []
 
