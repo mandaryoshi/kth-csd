0c79cb11a06e27a8a65713b71821957e46c67729
Enric Carrera Aguiar <enricca75@gmail.com>
2020-09-23 14:34:18 +0200

    traceroute file parsing

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 import json
-import ijson
 from tqdm import tqdm
 import time
 
@@ -14,14 +13,34 @@ import time
 json_data=[]
 id = 0
 traceroute_dict = {}
-with open('../traceroute-2020-09-14T1100','r') as readfile:
+with open('example_traceroute','r') as readfile:
 
     for line in readfile:
-        print(line)
-        time.sleep(1)
-        
-        for dataset in line:
-            print('\n')
+        json_line = json.loads(line)
+        #for dataset in json_line:
             #print(dataset)
             #print(line[1])
-    
+        if json_line["paris_id"]:
+            id = id + 1
+            traceroute_dict[id] = []
+            for item in json_line["result"]:
+                json_subitem = item['result']
+                rtt = 0
+                hop_ip = "x"
+                s = 0 
+                for item2 in json_subitem :
+                    if "from" in item2 :
+                        if item2["from"] != hop_ip :
+                            hop_ip = item2["from"]
+                        s = s+1    
+                        rtt = rtt + item2["rtt"]
+                        rtt_avg = rtt/s
+                if hop_ip != "x" and s != 0 :
+                    rtt_avgfinal = rtt_avg
+                    traceroute_dict[id].append({
+                        'hop': item['hop'], 
+                        'from' : hop_ip, 
+                        'rtt' : rtt_avgfinal})
+
+with open('traceroute_test.json', 'w') as fp:
+        json.dump(traceroute_dict,fp)
