e9aef2a2fe26424b688cb47c6cf2371282cd4319
Mandar <mandar1997@gmail.com>
2020-09-23 18:04:42 +0200

    Script A is done!
    So, using scripta.py we can provide a list of traceroutes and extract corresponding IXP id. To add: traceroute id and the IXP ip.
    
    Co-Authored-By: enriccia <enriccia@users.noreply.gits-15.sys.kth.se>
    Co-Authored-By: yulianti <yulianti@users.noreply.gits-15.sys.kth.se>

--- scripta.py
+++ scripta.py
@@ -4,32 +4,31 @@ import time
 
 #test = ["208.115.136.24","206.126.236.200","206.223.118.35"]
 
-#with open('ixp_test.json') as f:
-#  ixp_info = json.load(f)
+with open('ixp_test.json') as f:
+  ixp_info = json.load(f)
+
+def ixpdetection(ip_array):
+  for value,ixp in ixp_info.items():
+    #print(ixp["ipv4_prefix"])
+    for prefix in ixp["ipv4_prefix"]:
+      for ip in ip_array:
+        if IPAddress(ip) in IPNetwork(prefix):
+          print(value)
+          time.sleep(1)
 
 with open('traceroute_20200914_1100','r') as readfile:
-    id = 0
+    id = 1
     for line in readfile:
         
         
         json_line = json.loads(line)
         ip_array = []
-        print(json_line)
-        time.sleep(1)
+        #print(json_line)
+        #time.sleep(1)
 
-        """ for i in json_line[id]:
+        for i in json_line[str(id)]:
           ip_array.append(i["from"])
         ixpdetection(ip_array)
-        id = id + 1 """
-
-
-def ixpdetection(ip_array):
-  for value,ixp in ixp_info.items():
-    print(ixp["ipv4_prefix"])
-    for prefix in ixp["ipv4_prefix"]:
-      for ip in ip_array:
-        if IPAddress(ip) in IPNetwork(prefix):
-          print(value)
+        id = id + 1
 
 #ixpdetection(test)
-
