b7a364a488d70a3d8dd1f650535db80b14e95194
Enric Carrera Aguiar <enricca75@gmail.com>
2020-09-23 17:06:22 +0200

    script a

--- scripta.py
+++ scripta.py
@@ -2,10 +2,21 @@ from netaddr import IPNetwork, IPAddress
 import json 
 
 
-test = ["208.115.136.24","206.126.236.200","206.223.118.35"]
+#test = ["208.115.136.24","206.126.236.200","206.223.118.35"]
 
 with open('ixp_test.json') as f:
   ixp_info = json.load(f)
+
+with open('../traceroute-2020-09-14T1100','r') as readfile:
+    id = 0
+    for line in tqdm(readfile):
+        json_line = json.loads(line)
+        ip_array = []
+
+        for i in json_line[id]:
+          ip_array.append(i["from"])
+        ixpdetection(ip_array)
+
 
 def ixpdetection(ip_array):
   for value,ixp in ixp_info.items():
