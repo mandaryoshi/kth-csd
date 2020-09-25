80cf22770316f565e320e99228b75ec4d4712333
yulianti <yulianti@kth.se>
2020-09-25 21:39:22 +0200

    Script e done!
    Push the result into json format and put one condition to each ixp_index.
    
    Co-Authored-By: vargh <vargh@users.noreply.gits-15.sys.kth.se>

--- scripte.py
+++ scripte.py
@@ -3,35 +3,57 @@ import json
 import time
 from tqdm import tqdm
 
-#test = ["208.115.136.24","206.126.236.200","206.223.118.35"]
 
-with open('ixp_test.json') as f:
+with open('Documents/ixp_test.json') as f:
   ixp_info = json.load(f)
 
 def ixpdetection(ip_array):
   for value,ixp in ixp_info.items():
-    #print(ixp["ipv4_prefix"])
     for prefix in ixp["ipv4_prefix"]:
       for ip in ip_array:
         if IPAddress(ip) in IPNetwork(prefix):
-          #print(value)
           ixp_ip = IPAddress(ip)
           return ixp_ip
-          time.sleep(1)
+          #time.sleep(1)
 
-with open('example_traceroute_results','r') as readfile:
+file_object = open('Documents/example_hop_results', 'w')
+
+with open('Documents/example_traceroute_results','r') as readfile:
     id = 1
+   
     for line in readfile:
         json_line = json.loads(line)
         ip_array = []
-
+        iphop_dict = {}
+        iphop_dict[id] = []
         for i in json_line[str(id)]:
           ip_array.append(i["from"])  
         ixp_ip2 = ixpdetection(ip_array)
 #If an IXP ip is detected, save it along with its previous and next hop IP addresses.        
-        if ixp_ip2:
+        if ixp_ip2 :
             ixp_index = ip_array.index(str(ixp_ip2))
-            print("Prev Hop:", ip_array[ixp_index-1])
-            print("IXP Hop:", ip_array[ixp_index])
-            print("Next Hop:", ip_array[ixp_index+1])
+            if ixp_index-1 >= 0:
+              #print("Prev Hop:", ip_array[ixp_index-1])
+              previous_hop = ip_array[ixp_index-1]
+            elif ixp_index-1 < 0:
+              previous_hop = None
+            #print("IXP Hop:", ip_array[ixp_index])
+            current_hop = ip_array[ixp_index]
+            if ixp_index+1 <= len(ip_array)-1 :
+              #print("Next Hop:", ip_array[ixp_index+1])
+              next_hop = ip_array[ixp_index+1]
+            elif ixp_index+1 >= len(ip_array) :
+              #print("Next Hop:", 0)
+              next_hop = None
+            
+            iphop_dict[id].append({
+               'previous_hop' : previous_hop,
+               'ixp_hop' : current_hop,
+               'next_hop' : next_hop
+            })
+            #print(iphop_dict)
+            file_object.write(json.dumps(iphop_dict))
+            file_object.write('\n')
         id = id + 1
+
+file_object.close()
