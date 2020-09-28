1b1b0b39d54546dbd92e479901e0e7632ba2d312
Enric Carrera Aguiar <enricca75@gmail.com>
2020-09-28 11:08:27 +0200

    script a and c class

--- scriptc.py
+++ scriptc.py
@@ -1,11 +1,14 @@
 import json
 
-with open('ixp_test.json') as f:
-  ixp_info = json.load(f)
+class FacilityMapping:
+
+  def __init__(self):
+
+    with open('ixp_test.json') as f:
+      self.ixp_info = json.load(f)
+
+  def facility_search(self, ixp_id):
+    
+    return self.ixp_info[str(ixp_id)]["fac_set"]
 
 
-def facility_search(ixp_id):
-    print(ixp_info[str(ixp_id)]["fac_set"])
-
-
-facility_search(26)
