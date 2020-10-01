000486ce9f5e8379760f19e94e5bb4dd53a53f00
Mandar <mandar1997@gmail.com>
2020-10-01 15:15:30 +0200

    Finish changes to script e and a. Upload hop results.
    Co-Authored-By: enriccia <enriccia@users.noreply.gits-15.sys.kth.se>
    Co-Authored-By: vargh <vargh@users.noreply.gits-15.sys.kth.se>
    Co-Authored-By: yulianti <yulianti@users.noreply.gits-15.sys.kth.se>

--- scripta.py
+++ scripta.py
@@ -3,8 +3,6 @@ import time
 from tqdm import tqdm
 import pytricia
 
-#test = ["208.115.136.24","206.126.236.200","206.223.118.35"]
-
 class IxpDetector:
 
   def __init__(self):
@@ -15,34 +13,10 @@ class IxpDetector:
       for idval,ixp in self.ixp_info.items():
         for prefix in ixp["ipv4_prefix"]:
           self.pyt.insert(prefix, idval)
-          #print(prefix)
 
   def ixpdetection(self, ip_array):
     for ip in ip_array:
-      #if ip in self.pyt:
       idval = self.pyt.get(ip)
       if idval != None:
-        #print(ip, idval)
-        #time.sleep(1)
         return (ip, idval)
     return (None, None)
-
-
-#Ixp_detector().ixpdetection(test)
-
-
-
-"""with open('traceroute_20200914_1100','r') as readfile:
-    id = 1
-    for line in readfile:
-        json_line = json.loads(line)
-        ip_array = []
-        #print(json_line)
-        #time.sleep(1)
-
-        for i in json_line[str(id)]:
-          ip_array.append(i["from"])
-        ixpdetection(ip_array)
-        id = id + 1"""
-
-#ixpdetection(test)
--- scripte.py
+++ scripte.py
@@ -2,7 +2,7 @@ import json
 import time
 from tqdm import tqdm
 from scripta import IxpDetector
-import timeit
+#import timeit
 #start_time = timeit.default_timer()
 #print(timeit.default_timer() - start_time)
 
@@ -16,27 +16,28 @@ with open('json_results/traceroute_results','r') as readfile:
     for line in tqdm(readfile):
         json_line = json.loads(line)
         ip_array = []
+        hop_array = []
         iphop_dict = {}
        	iphop_dict["id"] = id
         for i in json_line[str(id)]:
           ip_array.append(i["from"])
+          hop_array.append(i["hop"])
 
         ixp_ip2, ixp_id = ix_detector.ixpdetection(ip_array)
-        #print(ixp_ip2, ixp_id)
-#        time.sleep(1)
     
 #If an IXP ip is detected, save it along with its previous and next hop IP addresses.        
         if ixp_ip2 :
             ixp_index = ip_array.index(str(ixp_ip2))
             
-            iphop_dict["data"] = {
-               'previous_hop' : ip_array[ixp_index-1] if (ixp_index-1 >= 0) else None,
-               'ixp_hop' : ip_array[ixp_index],
-               'next_hop' : ip_array[ixp_index+1] if (ixp_index+1 <= (len(ip_array)-1)) else None
-            }
-            #print(iphop_dict)
-            file_object.write(json.dumps(iphop_dict))
-            file_object.write('\n')
+            if ((ixp_index-1 >= 0) and (hop_array[ixp_index-1] == (hop_array[ixp_index] - 1))):
+              if ((ixp_index+1 <= (len(ip_array)-1)) and (hop_array[ixp_index] == (hop_array[ixp_index + 1] - 1))):
+                iphop_dict["data"] = {
+                  'previous_hop' : ip_array[ixp_index-1],
+                  'ixp_hop' : ip_array[ixp_index],
+                  'next_hop' : ip_array[ixp_index+1]
+                }
+                file_object.write(json.dumps(iphop_dict))
+                file_object.write('\n')
         id = id + 1
 
 file_object.close()
