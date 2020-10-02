da7315c70e1b215c7e8e648a8cec0195c8d98ed1
shbh <shbh@kth.se>
2020-10-02 13:58:46 +0200

    Final

---
+++ scriptb.py
@@ -0,0 +1,13 @@
+import json
+
+class IxpIP_AS_mapping:
+    def __init__(self):
+            with open('json_results/ixp_info_results.json') as f:
+                self.ixp_info = json.load(f)
+
+    def mapping(self, ixpip):
+        for ixp in self.ixp_info.values():
+            for addr,asn in ixp["net_set"].items():
+                for ip in ixpip:
+                    if ip == addr:
+                        return (asn)
---
+++ scriptd.py
@@ -0,0 +1,5 @@
+from netaddr import IPNetwork, IPAddress
+import json
+import time
+from tqdm import tqdm
+from scripta import IxpDetector
