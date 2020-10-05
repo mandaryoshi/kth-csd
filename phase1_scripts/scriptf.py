a80841ce39f289e727583c7bc85237d9af34196d
Mandar <mandar1997@gmail.com>
2020-10-05 13:51:16 +0200

    Update scriptf.py

--- phase1_scripts/scriptf.py
+++ phase1_scripts/scriptf.py
@@ -4,11 +4,13 @@ from tqdm import tqdm
 import ujson
 import networkx as nx
 import matplotlib.pyplot as plt
+import pathlib
 
 class IPNeighbors:
 
     def __init__(self):
-        self.trace_graph = nx.read_gpickle('../IK2200HT201-IXP/network_diagram/traceroute_graph.gpickle')
+        print(pathlib.Path.cwd())
+        self.trace_graph = nx.read_gpickle('network_diagram/traceroute_graph.gpickle')
         
     def graphgenerator(self, ip):
         neighbors = list(self.trace_graph.neighbors(ip))
@@ -16,26 +18,26 @@ class IPNeighbors:
         return neighbors
         
         
-        """
-        with open('json_results/hop_results','r') as readfile:
-            trace_graph = nx.read_gpickle('network_diagram/traceroute_graph.gpickle')
-            graph = nx.Graph()
+"""
+with open('json_results/hop_results','r') as readfile:
+    trace_graph = nx.read_gpickle('network_diagram/traceroute_graph.gpickle')
+    graph = nx.Graph()
 
-            hop_results = ujson.load(readfile)
-            count = 0
-            for key, hops in tqdm(hop_results.items()):
-                
-                neighbors = trace_graph.neighbors(hops["previous_hop"])
-                edges = []
-                for neighbor in neighbors:
-                    edges.append((hops["previous_hop"], neighbor))
-                    graph.add_edges_from(edges)
-                count = count + 1
-                if count == 3:
-                    break
-            nx.draw(graph, with_labels = True)
-            plt.show()
-        """
+    hop_results = ujson.load(readfile)
+    count = 0
+    for key, hops in tqdm(hop_results.items()):
+        
+        neighbors = trace_graph.neighbors(hops["previous_hop"])
+        edges = []
+        for neighbor in neighbors:
+            edges.append((hops["previous_hop"], neighbor))
+            graph.add_edges_from(edges)
+        count = count + 1
+        if count == 3:
+            break
+    nx.draw(graph, with_labels = True)
+    plt.show()
+"""
 
 ip_neighbors = IPNeighbors()
 ip_neighbors.graphgenerator('80.81.202.215')
