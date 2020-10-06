add4ffa77acefb4e8df3f9f43d44d56bc0cfa70f
shbh <shbh@kth.se>
2020-10-06 17:45:28 +0200

    First draft

--- nonixp_info.py
+++ nonixp_info.py
@@ -2,18 +2,30 @@ import json
 
 filename = 'json_results/example_nonixp.txt'
 
+#ip dictionary
 dict1 = {}
 
 fields = ['nonixpip','prefix_len','asn']
 
 #Creating dictionary for ip
 with open(filename) as fp:
-    l=1
+    a=1
     for line in fp:
         #Readling line by line from text file
-        description = list(line.strip().split(None,3))
-        print(description)
+        describe = list(line.strip().split(None,2))
+        #print(describe)
+        ip_no='ip'+str(a)
 
+        b=0
 
-with open('json_results/nonixp_info_results.json', 'w') as fp:
-json.dump(dict1, fp)
+        #fields dictionary
+        dict2 = {}
+        while b<3:
+            dict2[fields[b]] = describe[b]
+            b = b +1
+
+        dict1[ip_no] = dict2
+        a = a+1
+
+output_file = open("json_results/nonixp_info_results.json", "w")
+json.dump(dict1, output_file, indent = 4)
