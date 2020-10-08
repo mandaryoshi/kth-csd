ffadd8022cfe4651457e7f7c279c0fa2ac11834b
Mandar <mandar1997@gmail.com>
2020-10-08 09:51:53 +0200

    Update nonixp_info.py

--- nonixp_info.py
+++ nonixp_info.py
@@ -1,32 +1,24 @@
 import json
+from tqdm import tqdm
+import time
 
-filename = 'json_results/example_nonixp.pfx2as'
+filename = 'json_results/routeviews-rv2-20201005-1200.pfx2as'
 
 #nonixpip dictionary
 dict1 = {}
 
-fields = ['IP prefix','prefix_len','asn']
+fields = ['IP prefix','asn']
 
 #Creating dictionary for ip
 with open(filename) as fp:
-    a=1
-    for line in fp:
+    for line in tqdm(fp):
         #Readling line by line from text file
         describe = list(line.strip().split(None,2))
-
-        print(describe)
-        ip_no='ip'+str(a)
-
-        b=0
-
-        #nonixpip info dictionary
-        dict2 = {}
-        while b<3:
-            dict2[fields[b]] = describe[b]
-            b = b +1
-
-        dict1[ip_no] = dict2
-        a = a+1
+        describe[0:2] = [''.join(describe[0:2])]
+        string = describe[0] 
+        string = string[:-2] + '/' + string[-2:]
+        describe[0] = string
+        dict1[describe[0]] = describe[1]
 
 output_file = open("json_results/nonixp_info_results.json", "w")
 json.dump(dict1, output_file, indent = 4)
