113ceb18113ec37b1baa09abe5d007b9187c2d24
Mandar <mandarj@kth.se>
2020-10-28 21:30:21 +0100

    Update 14102020

--- phase2_scripts/new_method_near.py
+++ phase2_scripts/new_method_near.py
@@ -36,7 +36,7 @@ with open('../json_results/asn_fac_results.json') as f:
     asn_fac_info = ujson.load(f)
 
 #change the name of the folder
-with open("/home/csd/traceroutes/14092020/hop_results") as readfile:
+with open("/home/csd/traceroutes/14102020/hop_results") as readfile:
     hop_results = ujson.load(readfile)
     fac_ips = {}
     counter = 0
