fd5ad415632c0103e503b84d663a27f4aca79b21
CSD Student <student@csd.com>
2020-11-14 20:00:51 +0000

    graph

--- phase3_scripts/fw_link_monitoring.py
+++ phase3_scripts/fw_link_monitoring.py
@@ -7,6 +7,8 @@ from scipy.stats import chisquare
 import time
 import ast
 
+np.seterr(divide='ignore', invalid='ignore')
+
 date = sys.argv[1]
 hour = sys.argv[2]
 
@@ -82,7 +84,7 @@ for source in fw_dict.keys():
     if p_value > 0.01:
         r_val_dict = r_values(fw_dict[source])
         for dest in r_val_dict:
-            if r_val_dict[dest] > 0.25:
+            if r_val_dict[dest] < -0.25:
                 alarm_dict["alarms"].append((source, dest, r_val_dict[dest]))
 
 #save alarms and references
