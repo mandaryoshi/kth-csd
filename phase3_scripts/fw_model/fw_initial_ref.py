4df3ffac6123628e569763f465ea183deb4c5b0c
Enric Carrera Aguiar <enricca75@gmail.com>
2020-11-16 19:45:38 +0100

    Co-authored-by: mandarj<mandarj@kth.se>

--- phase3_scripts/fw_model/fw_initial_ref.py
+++ phase3_scripts/fw_model/fw_initial_ref.py
@@ -21,7 +21,7 @@ sdate = dt.date(int(split_date[0]), int(split_date[1]), int(split_date[2]))   # 
 hours = ["00","01","02","03","04","05","06","07","08","09","10","11","12","13",
          "14","15","16","17","18","19","20","21","22","23"]
 
-index_interval = ((int(hour)-3)%24,(int(hour)-1)%24)
+index_interval = ((int(hour)-3)%24,(int(hour)-2)%24,(int(hour)-1)%24)
 
 fw_dict = {}
 
