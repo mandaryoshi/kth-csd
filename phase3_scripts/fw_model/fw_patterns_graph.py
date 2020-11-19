840ac02fe3b75170e9c33f2240637094c7f1856f
CSD Student <student@csd.com>
2020-11-19 18:58:21 +0000

    test graphs

--- phase3_scripts/fw_model/fw_patterns_graph.py
+++ phase3_scripts/fw_model/fw_patterns_graph.py
@@ -135,5 +135,8 @@ plt.xticks(np.arange(len(date_list)),date_list,rotation='vertical')
 #plt.fill_between(np.arange(96), normal_reference_lower, normal_reference_upper, color='b', alpha=.1)
 
 #plt.errorbar(np.arange(96), rtt_median, yerr=err_list, fmt='o',capsize=5)
-save_path = "../results/fw_graph_" + source + "_msm.png"
+if dest_arg == None:
+    save_path = "../results/fw_graph_" + source + "_msm.png"
+else: 
+    save_path = "../results/fw_graph_" + source + "_" + dest_arg +  "_msm.png"
 plt.savefig(save_path)
