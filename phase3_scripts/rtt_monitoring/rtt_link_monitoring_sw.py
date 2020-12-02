95ced042dc1f5cd8ac14944f2b1da1b53da0a9cc
yulianti <yulianti@kth.se>
2020-12-02 20:04:50 +0100

    Commenting the codes
    Add comments to rtt_sw scripts

--- phase3_scripts/rtt_monitoring/rtt_link_monitoring_sw.py
+++ phase3_scripts/rtt_monitoring/rtt_link_monitoring_sw.py
@@ -1,3 +1,5 @@
+#This script is run to identify the alarm based on the difference between the current interval and 
+#initial reference interval
 import ujson 
 import sys
 from tqdm import tqdm
@@ -6,6 +8,7 @@ from math import sqrt
 import time
 from shutil import copyfile
 
+#Define wilson function to determine confidence interval
 def wilson(p, n, z = 1.96):
     denominator = 1 + z**2/n
     centre_adjusted_probability = p + z*z / (2*n)
@@ -17,6 +20,7 @@ def wilson(p, n, z = 1.96):
     return (round(lower_bound*n), round(upper_bound*n))
 
 
+#Give the current date and hour for link monitoring
 date = sys.argv[1]
 hour = sys.argv[2] 
 
@@ -37,34 +41,45 @@ copyfile(ref_path, save_path)
 median_dict = {}
 alarm_dict = {"alarms" : []}
 
+#Compute the current vaues
 for link in ref.keys():
+    #Only consider the link that consists of more than 5 traceroutes
     if link in links and len(links[link]["rtts"]) > 5:
+        #Sort the rtt list for each link, compute the median and the wilson scores
         sorted_rtts = sorted(links[link]["rtts"])
         normal_ref = np.median(sorted_rtts)
         ranks = wilson(0.5,len(sorted_rtts))
         #print(ranks, len(sorted_rtts))
+
+        #Define a confidence interval based on Wilson scores for each link (lower_bound, upper_bound)
         interval = (round(sorted_rtts[ranks[0]],5), round(sorted_rtts[ranks[1]],5))
+
+        #Fetch reference interval from initial reference computation
         ref_interval = (ref[link]["lower_bd"],ref[link]["upper_bd"])
+
+        #To detect the alarm, examine the difference between current interval and initial reference interval
+        #Use 1 ms as the threshold for difference
         if (((interval[0] - ref_interval[1]) > 1) or  ((ref_interval[0] - interval[1]) > 1)):
-            #currnt_lb - ref_ub or ref_lb - curr_ub
             #print("Alarm triggered!!", link, interval, ref_interval)
             alarm_dict["alarms"].append(link)
 
+        #Append the three items below to new dictionary as the current value (lower_bound, median, and upper_bound)
         median_dict[link] = {
             "lower_bd" : interval[0],
             "median" : normal_ref,
             "upper_bd" : interval[1]
         }
 
-
 ref_file.close()
+
+#Dump the current dictionary to a json file
 results_path = "/home/csd/traceroutes/" + date + "/" + hour + "/rtt_sw_medians"
 with open(results_path, 'w') as fp:
     ujson.dump(median_dict, fp)
 
 fp.close()
 
-
+#Dump alarm dictionary to a json file
 results_path = "/home/csd/traceroutes/" + date + "/" + hour + "/rtt_sw_alarms"
 with open(results_path, 'w') as fp:
     ujson.dump(alarm_dict, fp)
--- phase3_scripts/rtt_monitoring/rtt_monitoring_graph_sw.py
+++ phase3_scripts/rtt_monitoring/rtt_monitoring_graph_sw.py
@@ -1,3 +1,4 @@
+#This script is run to draw the graph that visualizes the rtt trend and alarm detections
 import ujson 
 import sys
 from tqdm import tqdm
@@ -5,12 +6,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 from datetime import date, timedelta
 
+#Use 24 hours a day for X-axis
 hours = ["00","01","02","03","04","05","06","07","08","09","10","11","12","13",
          "14","15","16","17","18","19","20","21","22","23"]
-         
+
+#Define start date and end date for the graph as input parameters        
 start_date = sys.argv[1].split('-')
 end_date = sys.argv[2].split('-')
 
+#Define source and destination links that want to monitor
 source = sys.argv[3]
 dest = sys.argv[4]
 
@@ -19,15 +23,16 @@ edate = date(int(end_date[0]), int(end_date[1]), int(end_date[2]))   # end date
 
 delta = edate - sdate       # as timedelta
 
-normal_reference_upper = []
-normal_reference_median = []
-normal_reference_lower = []
+#Create lists for errorbar
+normal_reference_upper = []     #initial reference upper_bound
+normal_reference_median = []    #initial reference median
+normal_reference_lower = []     #initial reference lower_bound
 
-rtt_upper = []
-rtt_median =  []
-rtt_lower = []
+rtt_upper = []      #current upper_bound
+rtt_median =  []    #current median
+rtt_lower = []      #current lower_bound
 
-date_list = []
+date_list = []  #list of dates
 
 for date in tqdm(range(delta.days + 1)):
     day = sdate + timedelta(days=date)   
@@ -35,50 +40,65 @@ for date in tqdm(range(delta.days + 1)):
         if hour == "00":
             date_list.append(str(day))
         else:
-            date_list.append(hour)
+            date_list.append(hour) 
+        #Open initial reference computation result
         file1 = open("/home/csd/traceroutes/" + str(day) + "/" + hour + "00/rtt_sw_ref_values")
         rtt_ref_values = ujson.load(file1)
         key = str((int(source), int(dest)))
+        #Append the value to each variable list
         if key in rtt_ref_values:
             normal_reference_upper.append(rtt_ref_values[key]["upper_bd"])
             normal_reference_lower.append(rtt_ref_values[key]["lower_bd"])
             normal_reference_median.append(rtt_ref_values[key]["median"])
 
-            #ref_intervallist.append([rtt_ref_values[key]["lower_bd"],rtt_ref_values[key]["upper_bd"]])
-
+            #Open current computation result
             file2 = open("/home/csd/traceroutes/" + str(day) + "/" + hour + "00/rtt_sw_medians")
             rtt_medians = ujson.load(file2)
             
-        
+            #Append the value to each variable list
             rtt_upper.append(rtt_medians[key]["upper_bd"] - rtt_medians[key]["median"])
             rtt_lower.append(rtt_medians[key]["median"] - rtt_medians[key]["lower_bd"])
             rtt_median.append(rtt_medians[key]["median"])
 
-            #rtt_intervallist.append([rtt_medians[key]["median"] - rtt_medians[key]["lower_bd"], rtt_medians[key]["upper_bd"] - rtt_medians[key]["median"]])
+            file3 = open("/home/csd/traceroutes/" + str(day) + "/" + hour + "00/rtt_sw_alarms")
+            alarms = ujson.load(file3)
+
+            if key in alarms["alarms"]:
+                alarm_list.append(len(date_list) -1)
 
             file2.close()
+            file3.close()
         else:
             print("INVALID LINK")
             sys.exit()
         
         file1.close()
 
+alarm_values = []
+for index in alarm_list:
+    alarm_values.append(rtt_median[index])
+
 #start graphing
 
 plt.figure(figsize=(30,10))
 
-plt.xticks(np.arange(168),date_list,rotation='vertical')
+plt.xticks(np.arange(len(date_list)),date_list,rotation='vertical')
 
 err_list = [rtt_lower, rtt_upper]
 
-plt.plot(np.arange(168), normal_reference_median, marker ='s')
+#Normal reference computation result
+plt.plot(np.arange(len(date_list)), normal_reference_median, marker ='.', color = 'forestgreen')
 #for x,y in zip(np.arange(216), normal_reference_median):
  #   label = y
  #   plt.annotate(label, (x,y), textcoords="offset points", xytext=(0,10), ha='center') 
 
-plt.fill_between(np.arange(168), normal_reference_lower, normal_reference_upper, color='b', alpha=.1)
+plt.fill_between(np.arange(len(date_list)), normal_reference_lower, normal_reference_upper, color='forestgreen', alpha=.1)
 
-plt.errorbar(np.arange(168), rtt_median, yerr=err_list, fmt='o',capsize=5)
+#Current computation result
+plt.errorbar(np.arange(len(date_list)), rtt_median, yerr=err_list, fmt='.',color='thistle',capsize=5)
+plt.plot(np.arange(len(date_list)), rtt_median, color='gold')
+
+plt.scatter(alarm_list, alarm_values, marker='X', color='magenta')
 #for x,y in zip(np.arange(216), rtt_median):
 #    label = y
 #    plt.annotate(label, (x,y), textcoords="offset points", xytext=(0,10), ha='center') 
