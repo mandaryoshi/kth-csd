52fc29b9d866867706a561b0e1a989f41b58fb11
shbh <shbh@kth.se>
2020-12-09 06:56:53 +0100

    Added comments

--- asn_fac.py
+++ asn_fac.py
@@ -2,12 +2,16 @@ import json
 import requests
 from tqdm import tqdm
 
+# Sending get request and saving the response as response object
 response = requests.get("https://peeringdb.com/api/netfac")
 
+# Parsing response
 netfac = json.loads(response.text)
 
+# Defining an empty dictionary
 dictionary = {
 
+# Iterating over key-'data' to map the asn and facility ids sequentially and append the facility ids in a list.
 for i in tqdm(netfac["data"]):
     if i["local_asn"] in dictionary:
         dictionary[i["local_asn"]].append(i["fac_id"])
@@ -15,5 +19,6 @@ for i in tqdm(netfac["data"]):
         dictionary[i["local_asn"]] = []
         dictionary[i["local_asn"]].append(i["fac_id"])
 
+# Opens the file where dictionary will be dumped
 with open('json_results/asn_fac_results.json', 'w') as fp:
         json.dump(dictionary,fp)
--- fac_loc.py
+++ fac_loc.py
@@ -2,12 +2,16 @@ import ujson
 import requests
 from tqdm import tqdm
 
+# Sending get request and saving the response as response object
 response = requests.get("https://peeringdb.com/api/fac")
 
+# Parsing response
 netfac = ujson.loads(response.text)
 
+# Defining an empty dictionary
 dictionary = {}
 
+# Iterating over key-'data' sequentially to map the facility ids with their latitude, longitude and name
 for i in tqdm(netfac["data"]):
     if i["latitude"] != None and i["longitude"] != None:
         dictionary[i["id"]]  =  {
@@ -16,6 +20,6 @@ for i in tqdm(netfac["data"]):
             "name" : i["name"]
         }
        
-
+# Opens the file where dictionary will be dumped
 with open('json_results/fac_loc_results.json', 'w') as fp:
         ujson.dump(dictionary,fp)
